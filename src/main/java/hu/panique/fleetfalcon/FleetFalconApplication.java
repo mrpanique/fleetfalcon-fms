492a9303c3bd51b6885102bf8ade583e6131d400
panique <balint.panyik@gmail.com>
2026-02-09 00:21:58 +0100

    spring boot initialization, db setup in docker compose and spring boot properties configuration, lombok added to pom.xml dependencies

---
+++ src/main/java/hu/panique/fleetfalcon/FleetFalconApplication.java
@@ -0,0 +1,13 @@
+package hu.panique.fleetfalcon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetFalconApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetFalconApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/panique/fleetfalcon/FleetFalconApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.panique.fleetfalcon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetFalconApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
