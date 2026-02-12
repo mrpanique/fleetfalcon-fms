7142d3bcdee31401c50df7105e81581984c7a4e4
panique <balint.panyik@gmail.com>
2026-02-12 23:57:47 +0100

    feat: add EmployeeController, EmployeeRepository and delete unused imports from FleetFalconApplication

--- src/main/java/hu/panique/fleetfalcon/FleetFalconApplication.java
+++ src/main/java/hu/panique/fleetfalcon/FleetFalconApplication.java
@@ -1,11 +1,7 @@
 package hu.panique.fleetfalcon;
 
-import hu.panique.fleetfalcon.model.Vehicle;
-import hu.panique.fleetfalcon.repository.VehicleRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FleetFalconApplication {
